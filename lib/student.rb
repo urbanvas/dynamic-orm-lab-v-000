c78d31959edf0a28f3992dc6a68b561d6728f765
Neil Seejoor <nvseejoor@gmail.com>
2018-04-02 20:33:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,42 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
-require 'interactive_record.rb'
 
-class Student < InteractiveRecord
+class InteractiveRecord
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if { |col| col == "id"}.join(", ")
+  end
+
+
+
 
 end
